5720213564731c94a7528b74b9a9feb968a5d446
budy6991 <alvmenfer@gmail.com>
2022-08-03 18:26:30 +0200

    Test for calculator working

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,14 @@
 const calculator = require('./calculator')
 
 test('Properly adds two numbers', () => {
-    expect(calculator.add(2,3)).toBe(5)
+    expect(calculator.add(2,3)).toEqual(5)
+})
+test('Properly substracts two numbers', () => {
+    expect(calculator.subtract(5,2)).toEqual(3)
+})
+test('Properly divides two numbers', () => {
+    expect(calculator.divide(6,2)).toEqual(3)
+})
+test('Properly multiplies two numbers', () => {
+    expect(calculator.multiply(2,3)).toEqual(6)
 })
